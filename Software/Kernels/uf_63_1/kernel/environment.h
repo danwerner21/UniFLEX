14c05d0b2dfe255b19528292809c7658e37303e1
Kees Schoenmakers <kslinux@gmail.com>
2019-09-26 23:01:29 +0200

    Add files via upload
    First entry. These are the 'core' kernel files. All in UniFLEX format

---
+++ Software/Kernels/uf_63_1/kernel/environment.h
@@ -0,0 +1,1 @@
+  lib ../include//title.h  opt nol  lib ../include/params.h  lib ../include/macdefs.h  lib ../include/struct.h  lib ../include/varbls.h  lib ../include/systabs.h  lib ../include/sysdefs.h  data  opt lis
